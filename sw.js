b4c96d9462b432bf4e0d55724a375d5015a7ff31
pburd <p.burda2003@gmail.com>
2025-05-09 15:13:06 +0200

    edit

--- sw.js
+++ sw.js
@@ -2,14 +2,18 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
   '/index.html',
+  '/map.html',
+  '/sos.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+  "/images/hello-icon-192.png",
+  "/images/hello-icon-512.png"
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
@@ -17,9 +21,9 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
